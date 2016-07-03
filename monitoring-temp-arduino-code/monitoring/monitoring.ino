0216cb039036b5bb4238ff0cb0818dbb1431c62e
nitram509 <maki@bitkings.de>
2016-07-03 15:17:47 +0200

    add pictures and refactored code to use ringBuffer

---
+++ monitoring-temp-arduino-code/monitoring/monitoring.ino
@@ -0,0 +1,93 @@
+
+
+/**************************************************************/
+/* define pin setup, what sensor/fan is connected to what pin */
+/**************************************************************/
+
+int LM35_1_PIN = A0;
+int LM35_2_PIN = A1;
+int FAN_PWM_PIN = 9;
+
+#define SEND_TEMP_VALUES_TO_HOST true
+#define SEND_TO_HOST_INTERVALL_IN_MILLIS 1000
+
+/***************************************************************/
+
+#define RING_BUFFER_SIZE 5
+#define INVALID_VALUE -273.0
+#define WAIT_MILLIS (SEND_TO_HOST_INTERVALL_IN_MILLIS / RING_BUFFER_SIZE)
+
+/***************************************************************/
+
+struct TemperatureRingBuffer {
+    float values[RING_BUFFER_SIZE];
+    int writeIndex = 0;
+};
+
+TemperatureRingBuffer tempRingBuf_1;
+TemperatureRingBuffer tempRingBuf_2;
+unsigned long lastSend = 0;
+
+/**************************************************************/
+
+float readTemperatureInCelcius(int lm35_pin) {
+    return (5.0 * 100.0 * analogRead(lm35_pin)) / 1024;
+}
+
+void buffer_write(TemperatureRingBuffer *tempRingBuf, float value) {
+    tempRingBuf->values[tempRingBuf->writeIndex] = value;
+    tempRingBuf->writeIndex = (tempRingBuf->writeIndex++) % RING_BUFFER_SIZE;
+}
+
+void initRingBuffer(TemperatureRingBuffer *tempRingBuf) {
+    for (int i=0; i<RING_BUFFER_SIZE; i++) {
+        tempRingBuf->values[i] = INVALID_VALUE;
+    }
+}
+
+float averageValues(TemperatureRingBuffer *tempRingBuf) {
+    float avg = 0.0;
+    int counter = 0;
+    for (int i=0; i<RING_BUFFER_SIZE; i++) {
+        if (tempRingBuf->values[i] != INVALID_VALUE) {
+            counter ++;
+            avg += tempRingBuf->values[i];
+        }
+    }
+    return avg / (float)counter;
+}
+
+void setup() {
+    if (SEND_TEMP_VALUES_TO_HOST) {
+        Serial.begin(9600);
+    }
+
+    pinMode(FAN_PWM_PIN, OUTPUT);
+
+    initRingBuffer(&tempRingBuf_1);
+    initRingBuffer(&tempRingBuf_2);
+}
+
+void loop() {
+
+    buffer_write(&tempRingBuf_1, readTemperatureInCelcius(LM35_1_PIN));
+    buffer_write(&tempRingBuf_2, readTemperatureInCelcius(LM35_2_PIN));
+    delay(WAIT_MILLIS);
+
+    unsigned long currentMillis = millis();
+    if (SEND_TEMP_VALUES_TO_HOST && (currentMillis - lastSend) > SEND_TO_HOST_INTERVALL_IN_MILLIS) {
+        lastSend = currentMillis;
+
+        Serial.print("(1) ");
+        Serial.print(averageValues(&tempRingBuf_1), 1);
+        Serial.println("Grad Celsius");
+
+        Serial.print("(2) ");
+        Serial.print(averageValues(&tempRingBuf_2), 1);
+        Serial.println("Grad Celsius");
+
+        Serial.println("");
+    }
+
+    analogWrite(FAN_PWM_PIN, 255);
+}
